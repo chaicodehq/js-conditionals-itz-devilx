1302a019fdab4b191c1bf534b4a52f219191e3b9
itz-devilx <intekhab118211989@gmail.com>
2026-02-10 02:59:07 +0530

    implement season activity based on month and temperature

--- src/12-season-activity.js
+++ src/12-season-activity.js
@@ -31,5 +31,45 @@
  * @returns {{ season: string, activity: string } | null}
  */
 export function getSeasonActivity(month, temperature) {
-  // Your code here
+  let season = "";
+  if(month === 12 || month === 1 || month === 2){
+    season = "Winter";
+  }
+  else if(month === 3 || month === 4 || month === 5){
+    season = "Spring"
+  }
+  else if(month === 6 || month === 7 || month === 8){
+    season = "Summer"
+  }
+  else if(month === 9 || month === 10 || month === 11){
+    season = "Autumn"
+  }else{
+    return null;
+  }
+
+  if(season == 'Winter' && temperature < 0){
+    return {season, activity: "skiing"}
+  }
+  else if(season == 'Winter' && temperature >= 0){
+    return {season, activity: "ice skating"}
+  }
+  else if(season == "Spring" && temperature > 20){
+    return {season, activity: "hiking"}
+  }
+  else if(season == "Spring" && temperature <= 20){
+    return {season, activity: "museum visit"}
+  }
+  else if(season == "Summer" && temperature > 35){
+    return {season, activity: "swimming"}
+  }
+  else if(season == "Summer" && temperature <= 35){
+    return {season, activity: "cycling"}
+  }
+  else if(season == "Autumn" && temperature > 15){
+    return {season, activity: "nature walk"}
+  }
+  else if(season == "Autumn" && temperature <= 15){
+    return {season, activity: "reading at a cafe"}
+  }
+  
 }
